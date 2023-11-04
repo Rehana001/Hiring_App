764e721049cdbb4b900841d78bdceae27fe5397e
Rehana <rehanaNoorani3@gmail.com>
2023-11-04 15:55:00 +0500

    drawer_in_hiring

--- hiring_app/App.js
+++ hiring_app/App.js
@@ -7,31 +7,43 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Hiring_Page from './src/Hiring_Page';
 import Hiring_Screen from './src/Hiring_Screen';
 import Providing_Screen from './src/Providing_Screen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './src/Hiring/Home';
+import Saved from './src/Hiring/Saved';
 
+const Stack= createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 
-  const Stack= createStackNavigator();
-
   return (
-
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="Hiring" component={Hiring_Page}/>
-        <Stack.Screen name="Hiring_Screen" component={Hiring_Screen}/>
-        <Stack.Screen name="Providing_Screen" component={Providing_Screen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-    // <View style={styles.container}>
-    //  <Login/>
-    //   {/* <Register/> */}
-    //   {/* <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" /> */}
-    // </View>
+      <HiringScreens/>
+    {/* <DrawerNavigator /> */}
+  </NavigationContainer>
   );
 }
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Saved" component={Saved} />
+    </Drawer.Navigator>
+  );
+}
+
+function HiringScreens() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="Register" component={Register}/>
+      <Stack.Screen name="Hiring" component={Hiring_Page} />
+      <Stack.Screen name="Hiring_Screen" component={Hiring_Screen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Providing_Screen" component={Providing_Screen} />
+    </Stack.Navigator>
+  )
+  };
 
 const styles = StyleSheet.create({
   container: {
--- hiring_app/src/Hiring_Screen.js
+++ hiring_app/src/Hiring_Screen.js
@@ -1,18 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {View, Text} from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './Hiring/Home';
+import Saved from './Hiring/Saved';
+import Messages from './Hiring/Messages';
+import Profile from './Hiring/Profile';
+import { Ionicons, MaterialCommunityIcons } from 'react-native-vector-icons';
+import { StyleSheet } from 'react-native';
 
+const Drawer = createDrawerNavigator();
 
-
-const Hiring_Screen = () => {
+function Hiring_Screen() {
   return (
-    // <Drawer.Navigator>
-    //   <Drawer.Screen name="Home" component={Home} />
-    //   <Drawer.Screen name="Saved" component={Saved} />
-    // </Drawer.Navigator>
-    <Text> Hiring_Screen</Text>
-  )
+    
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Saved"
+        component={Saved}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="save" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Messages"
+        component={Messages}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="message" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+   
+  );
 }
 
-export default Hiring_Screen
+export default Hiring_Screen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+        container: {
+            flexGrow: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }
+})
