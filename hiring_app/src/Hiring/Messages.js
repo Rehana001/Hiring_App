98659b49862463f68d8f3020e722cf016754a81c
Rehana <rehanaNoorani3@gmail.com>
2023-11-11 15:01:36 +0500

    ProfileScreen

--- hiring_app/src/Hiring/Messages.js
+++ hiring_app/src/Hiring/Messages.js
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Messages = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSend = () => {
+    // Add logic here to handle sending the message
+    console.log('Sending message:', inputValue);
+    // You can clear the input after sending if needed
+    setInputValue('');
+  };
+
   return (
-    <View>
-      <Text>Messages</Text>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.messageInput}
+          placeholder='Message here'
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+        />
+         <FontAwesome5Icon
+          name="paper-plane"
+          style={styles.sendIcon}
+          onPress={handleSend}
+        />
+       
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  messageInput: {
+    backgroundColor: '#dce0e0',
+    flex: 1,
+    margin: 15,
+    paddingTop: 6,
+    paddingLeft: 5,
+    paddingBottom: 50,
+    borderRadius: 5,
+    height: 100,
+  },
+  sendIcon: {
+    fontSize: 24,
+    color: 'purple',
+    marginHorizontal: 4,
+    paddingRight: 5,
+  },
+});
--- hiring_app/src/Hiring/Profile.js
+++ hiring_app/src/Hiring/Profile.js
@@ -1,14 +1,117 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Linking, Image } from 'react-native';
+import React from 'react';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 const Profile = () => {
+  const emailAddress = 'abc@gmail.com';
+  const MobileNumber = '0332336495';
+  const websiteUrl = 'https://example.com';
+
+  const handleEmailPress = () => {
+    const emailLink = `mailto:${emailAddress}`;
+    Linking.openURL(emailLink);
+  };
+
+  const handleMobilePress = () => {
+    const phoneLink = `tel:${MobileNumber}`;
+    Linking.openURL(phoneLink);
+  };
+
+  const handleWebsitePress = () => {
+    Linking.openURL(websiteUrl);
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <Image
+        style={{ width: 350, height: 300 }}
+        source={require('../../assets/Images/minimum-viable-team.jpg')}
+      />
+      <View style={styles.ProfileView}>
+        <Text style={styles.ContactText}>Contact Information</Text>
+        <View style={styles.infoContainer}>
+          <FontAwesomeIcon name="envelope" style={styles.icon} />
+          <Text style={styles.HeadingText}>Email Address: </Text>
+          <Text onPress={handleEmailPress} style={styles.emailText}>
+            {emailAddress}
+          </Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <FontAwesomeIcon name="phone" style={styles.icon} />
+          <Text style={styles.HeadingText}>Mobile Number: </Text>
+          <Text onPress={handleMobilePress} style={styles.MobileText}>
+            {MobileNumber}
+          </Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <FontAwesomeIcon name="globe" style={styles.icon} />
+          <Text style={styles.HeadingText}>Website: </Text>
+          <Text onPress={handleWebsitePress} style={styles.websiteText}>
+            {websiteUrl}
+          </Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <FontAwesomeIcon name="info" style={styles.icon} />
+          <Text style={styles.HeadingText}>About Me:</Text>
+        </View>
+        <Text style={styles.contentText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+   
+  },
+  emailText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    padding: 6,
+  },
+  MobileText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    padding: 6,
+  },
+  websiteText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    padding: 6,
+  },
+  ProfileView: {
+    padding: 10,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  ContactText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    padding: 6,
+  },
+  HeadingText: {
+    fontSize: 15,
+    padding: 6,
+    fontWeight: '500',
+  },
+  contentText: {
+    margin: 6,
+    padding: 6,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 6,
+  },
+  icon: {
+    fontSize: 20,
+    color: '#0c99a8',
+    marginRight: 6,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Profile;
